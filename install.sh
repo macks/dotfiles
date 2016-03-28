2550ec98f650dc05f35fe6b684f0388125d21807
MATSUYAMA Kengo <macksx@gmail.com>
2016-03-28 16:07:12 +0900

    Initial import.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+myname=$(basename "$0")
+mydir=$(readlink -f "$(dirname "$0")")
+
+for f in $(ls -a "$mydir"); do
+  case "$f" in
+  .|..|.git|$myname|*.swp|*~)
+    continue
+  esac
+
+  symlink="$HOME/$f"
+  test -e "$symlink" -a ! -L "$symlink" && continue
+
+  echo "install $f"
+  ln -sfn "$mydir/$f" "$symlink"
+done
