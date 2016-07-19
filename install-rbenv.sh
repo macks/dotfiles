be62ff4c1f1eb2c3cecc20fac75633e48e88f4b6
MATSUYAMA Kengo <k-matsuyama@bns-g.com>
2016-07-19 20:12:11 +0900

    Rewrite install-rbenv.sh

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 RBENV_ROOT=${1:-~/.rbenv}
 
+RBENV_PLUGINS="
+https://github.com/rbenv/ruby-build.git
+https://github.com/sstephenson/rbenv-default-gems.git
+https://github.com/rkh/rbenv-update.git
+"
+
 if test -d "$RBENV_ROOT"; then
   echo "Already exists: $RBENV_ROOT"
   exit 1
@@ -8,8 +14,8 @@ fi
 
 git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
 
-git clone https://github.com/rbenv/ruby-build.git               "$RBENV_ROOT/plugins/ruby-build"
-git clone https://github.com/sstephenson/rbenv-default-gems.git "$RBENV_ROOT/plugins/rbenv-default-gems"
-git clone https://github.com/rkh/rbenv-update.git               "$RBENV_ROOT/plugins/rbenv-update"
+for url in $RBENV_PLUGINS; do
+  git clone $url "$RBENV_ROOT/plugins/$(basename "$url" .git)"
+done
 
 echo bundler > "$RBENV_ROOT/default-gems"
