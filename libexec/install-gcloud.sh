2bf70020cd6c7ed7264b805521a822c2d72f3fba
MATSUYAMA Kengo <macksx@gmail.com>
2018-08-09 20:28:19 +0900

    install-gcloud: fix os

--- libexec/install-gcloud.sh
+++ libexec/install-gcloud.sh
@@ -6,7 +6,8 @@ if test -d ~/google-cloud-sdk; then
 fi
 
 version=$(curl -sL curl -sL https://cloud.google.com/sdk/docs/release-notes | perl -lne 'if (/^<h2[^>]*>([^ ]+) [^ ]*<\/h2>$/) { print $1; exit }')
-curl -sL -o /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-linux-x86_64.tar.gz
+os=$(uname | tr A-Z a-z)
+curl -L -o /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-${os}-x86_64.tar.gz
 
 tar -C ~ -xzf /tmp/gcloud.tar.gz
 rm -f /tmp/gcloud.tar.gz
