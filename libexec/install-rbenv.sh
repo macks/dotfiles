5e28e4248a3d4c1bee67b846044f8a9edc76c073
MATSUYAMA Kengo <macksx@gmail.com>
2018-12-11 16:23:58 +0900

    rbenv: Don't install bundler gem automatically.

--- libexec/install-rbenv.sh
+++ libexec/install-rbenv.sh
@@ -3,7 +3,6 @@ RBENV_ROOT=${1:-~/.rbenv}
 
 RBENV_PLUGINS="
 https://github.com/rbenv/ruby-build.git
-https://github.com/sstephenson/rbenv-default-gems.git
 https://github.com/rkh/rbenv-update.git
 "
 
@@ -17,5 +16,3 @@ git clone --depth 1 https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
 for url in $RBENV_PLUGINS; do
   git clone --depth 1 $url "$RBENV_ROOT/plugins/$(basename "$url" .git)"
 done
-
-echo bundler > "$RBENV_ROOT/default-gems"
